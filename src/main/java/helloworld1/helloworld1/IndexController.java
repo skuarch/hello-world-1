54d26bed9ffc40e6b64c094254eedf637d60720e
skuarch <skuarch@yahoo.com.mx>
2019-06-30 17:04:46 -0500

    New index controller

---
+++ src/main/java/helloworld1/helloworld1/IndexController.java
@@ -0,0 +1,16 @@
+package helloworld1.helloworld1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    
+    @GetMapping
+    public String index() {
+        return "Hello world";
+    }
+    
+}
